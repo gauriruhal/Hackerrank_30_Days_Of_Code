148308eedf13cc5e8d3eb6e4df8686ab8981b292
Gauri <gauriruhal9@gmail.com>
2021-05-21 15:32:45 +0530

    30 Days of Code

---
+++ Day 0 helloworld/Day0 hello,world.py
@@ -0,0 +1,8 @@
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+INPUT_STRING = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# Write a line of code here that prints the contents of input_string to stdout.
+print(INPUT_STRING)
